5a7c26c771017bcc3f2766344052dee0dce086c6
Fayiz Musthafa <fayiz.musthafa@livestream.com>
2013-09-15 18:51:46 +0530

    Added dist utils

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='pytablefy',
+      version=version,
+      description="A python SVG table library",
+      long_description="""\
+A python SVG table library""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python table SVG PNG',
+      author='Fayiz Musthafa',
+      author_email='fayizk1@gmail.com',
+      url='',
+      license='MIT/X11 license',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
