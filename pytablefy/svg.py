a9bf421c210ab684886f6dd221558f2148175577
Fayiz Musthafa <fayiz.musthafa@livestream.com>
2013-09-18 19:14:50 +0530

    some chnage

--- pytablefy/svg.py
+++ pytablefy/svg.py
@@ -2,5 +2,9 @@
 from xml.etree import ElementTree as et
 
 class Svg(object):
-    def __init__(self):
-        self.svgdata = 
+    def __init__(self, width, height, background):
+        self.width = str(width)
+        self.height = str(height)
+        self.background = background
+        self.svgdata = et.Element('svg', width=self.width, height=self.height, version='1.1', xmlns='http://www.w3.org/2000/svg')
+        self.rect(self.background)
